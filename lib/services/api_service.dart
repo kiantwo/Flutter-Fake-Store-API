42825997f38ed8da9ad90d086a03d4de8607ec14
Kian <shanekiantiu@gmail.com>
2022-07-12 11:15:20 +0800

    Add Get All Products

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,3 +1,22 @@
+import 'dart:convert';
+
+import 'package:fake_store/models/product.dart';
+import 'package:http/http.dart' as http;
+
 class ApiService {
   static const String baseUrl = 'https://fakestoreapi.com';
+
+  Future<List<Product>> getAllProducts() async {
+    return http.get(Uri.parse('$baseUrl/products')).then((data) {
+      final products = <Product>[];
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+
+        for (var product in jsonData) {
+          products.add(Product.fromJson(product));
+        }
+      }
+      return products;
+    }).catchError((err) => print(err));
+  }
 }
