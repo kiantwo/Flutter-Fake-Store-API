bed27793f3e233633aa8eb89f8712875fe87b7d4
Kian <shanekiantiu@gmail.com>
2022-07-12 15:40:35 +0800

    Add User Login Functionality

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,3 +1,20 @@
+import 'dart:convert';
+
+import 'package:fake_store/models/user_login.dart';
+import 'package:http/http.dart' as http;
+
 class ApiService {
   static const String baseUrl = 'https://fakestoreapi.com';
+
+  Future<dynamic> login(String username, String password) {
+    final credentials = UserLogin(username: username, password: password);
+    return http
+        .post(Uri.parse('$baseUrl/auth/login'), body: credentials.toJson())
+        .then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        return jsonData;
+      }
+    }).catchError((err) => print(err));
+  }
 }
